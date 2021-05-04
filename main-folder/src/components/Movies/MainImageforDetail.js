d9df6d79994f16b32c2b00addcac2ca287b1eb4b
abc <prajapatijimil@yahoo.com>
2021-05-04 16:39:14 +0530

    Jimil: brushing up the last commit

--- main-folder/src/components/Movies/MainImageforDetail.js
+++ main-folder/src/components/Movies/MainImageforDetail.js
@@ -4,20 +4,19 @@ import Fade from 'react-reveal/Fade';
 function MainImageforDetail(props) {
     return (
         <Fade top>
-        <div style={{
-                    backgroundImage:`url('${props.image}')`,
-                    height: '75vh',
-                    position: 'relative',
-                    backgroundSize:'cover'
-                }}>
-        <div>
-            <div style={{ position: 'absolute', maxWidth: '500px', bottom: '2rem', marginLeft: '2rem' }}>
-                <div style={{ color: 'white' }} className="h2 font-weight-bolder" > {props.title}</div>
-                <p style={{ color: 'white', fontSize: '1rem' }}>{props.text}</p>
+            <div style={{
+                        backgroundImage: `url('${props.image}'), linear-gradient(to bottom, rgba(0,0,0,0)39%,rgba(0,0,0,0)41%,rgba(0,0,0,0.65)100%)`,
+                        height: '75vh',
+                        position: 'relative',
+                        backgroundSize:'cover'
+            }}>
+                <div>
+                    <div style={{ position: 'absolute', maxWidth: '500px', bottom: '2rem', marginLeft: '2rem' }}>
+                        <div style={{ color: 'white' }} className="h2 font-weight-bolder" > {props.title}</div>
+                        <p style={{ color: 'white', fontSize: '1rem' }}>{props.text}</p>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        </div>
         </Fade>
     )
 }
